bf626b2398cba9b5e5278e8c49b8e63ed1fe1904
sreesanth <sreesanthn970@gmail.com>
2026-03-02 04:08:13 +0530

    cursor

--- src/components/CustomCursor.jsx
+++ src/components/CustomCursor.jsx
@@ -1,47 +1,65 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef } from "react";
 import { createPortal } from "react-dom";
+import { gsap } from "gsap";
 
-function CursorUI({ pos }) {
-  return (
+function CustomCursor() {
+  const innerDotRef = useRef(null);
+  const outerRingRef = useRef(null);
+
+  useEffect(() => {
+    const innerDot = innerDotRef.current;
+    const outerRing = outerRingRef.current;
+
+    if (!innerDot || !outerRing) return;
+
+    // Set initial positions and centering
+    gsap.set([innerDot, outerRing], { xPercent: -50, yPercent: -50 });
+
+    const moveCursor = (e) => {
+      const { clientX, clientY } = e;
+      
+      // Inner dot moves instantly
+      gsap.to(innerDot, {
+        x: clientX,
+        y: clientY,
+        duration: 0,
+      });
+
+      // Outer ring has a slight delay for a smooth trailing effect
+      gsap.to(outerRing, {
+        x: clientX,
+        y: clientY,
+        duration: 0.3,
+        ease: "power3.out",
+      });
+    };
+
+    window.addEventListener("mousemove", moveCursor);
+
+    return () => {
+      window.removeEventListener("mousemove", moveCursor);
+    };
+  }, []);
+
+  return createPortal(
     <>
       {/* Outer ring */}
       <div
-        className="fixed pointer-events-none z-[999999]
+        ref={outerRingRef}
+        className="fixed pointer-events-none z-[999999] top-0 left-0
                    w-8 h-8 rounded-full
                    border border-primary/60"
-        style={{
-          left: pos.x,
-          top: pos.y,
-          transform: "translate(-50%, -50%)",
-        }}
       />
 
       {/* Inner dot */}
       <div
-        className="fixed pointer-events-none z-[999999]
+        ref={innerDotRef}
+        className="fixed pointer-events-none z-[999999] top-0 left-0
                    w-3 h-3 rounded-full bg-primary"
-        style={{
-          left: pos.x,
-          top: pos.y,
-          transform: "translate(-50%, -50%)",
-        }}
       />
-    </>
+    </>,
+    document.body
   );
-}
-
-function CustomCursor() {
-  const [pos, setPos] = useState({ x: 0, y: 0 });
-
-  useEffect(() => {
-    const move = (e) => {
-      setPos({ x: e.clientX, y: e.clientY });
-    };
-    document.addEventListener("mousemove", move);
-    return () => document.removeEventListener("mousemove", move);
-  }, []);
-
-  return createPortal(<CursorUI pos={pos} />, document.body);
 }
 
 export default CustomCursor;
